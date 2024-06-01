ab2eed616fa36c42bef86feb84c76bb6a57edf3f
shimfff <shimseawon1510@gmail.com>
2024-06-01 21:36:59 +0900

    [Feature]: ApiResponse 수정

--- src/main/java/umc/dofarming/domain/file/controller/FileController.java
+++ src/main/java/umc/dofarming/domain/file/controller/FileController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import umc.dofarming.api_response.ApiResponse;
 import umc.dofarming.domain.file.dto.FileCreateResponse;
 import umc.dofarming.domain.file.service.FileService;
 
@@ -21,17 +22,21 @@ public class FileController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "파일 생성 API")
-    public FileCreateResponse createFile(
+    public ApiResponse<FileCreateResponse> createFile
+        (
             @Valid @Parameter(
                     content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE) )
-            @RequestPart("file") MultipartFile file) {
-        return fileService.createFile("test", file);
+            @RequestPart("file") MultipartFile file
+        )
+    {
+        return ApiResponse.onSuccess(fileService.createFile("", file));
     }
 
     @DeleteMapping
     @Operation(summary = "파일 삭제 API")
-    public void deleteFile(@RequestParam("fileUrl") String fileUrl) {
+    public ApiResponse<String> deleteFile(@RequestParam("fileUrl") String fileUrl) {
         fileService.deleteFile(fileUrl);
+        return ApiResponse.onSuccess("파일 삭제 성공");
     }
 }
 
