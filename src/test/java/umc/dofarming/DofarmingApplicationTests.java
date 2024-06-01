10343803e3791691be9e1bc2e99ff9b734e40199
Kim Doo Hyeon <engus525@naver.com>
2024-06-01 13:48:18 +0900

    Initial Commit

---
+++ src/test/java/umc/dofarming/DofarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.dofarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofarmingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
