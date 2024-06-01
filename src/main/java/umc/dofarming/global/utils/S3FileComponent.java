f3af330c25465a062b38c090c5b86ebda193dd66
shimfff <shimseawon1510@gmail.com>
2024-06-01 18:27:40 +0900

    [Feature]: S3 로그 추가

--- src/main/java/umc/dofarming/global/utils/S3FileComponent.java
+++ src/main/java/umc/dofarming/global/utils/S3FileComponent.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +18,7 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class S3FileComponent {
     private final AmazonS3Client amazonS3Client; // AmazonS3Client 객체 주입
     @Value("${cloud.aws.s3.bucket}") // S3 버킷 이름
@@ -37,7 +39,7 @@ public class S3FileComponent {
         try {
             bytes = multipartFile.getBytes();
         } catch (IOException e) {
-            // 파일 업로드 실패 시 에러 코드 분기
+            // 파일 읽기 실패
             if (isImage) {
                 throw new GeneralException(ErrorStatus.FALIED_READ_IMAGE);
             } else {
@@ -58,8 +60,10 @@ public class S3FileComponent {
         } catch (AmazonS3Exception e) {
             // S3 업로드 실패 시 에러 코드 분기
             if (isImage) {
+                log.error(e.getMessage());
                 throw new GeneralException(ErrorStatus.FAILED_UPLOAD_S3_IMAGE);
             } else {
+                log.error(e.getMessage());
                 throw new GeneralException(ErrorStatus.FAILED_UPLOAD_S3_FILE);
             }
         }
